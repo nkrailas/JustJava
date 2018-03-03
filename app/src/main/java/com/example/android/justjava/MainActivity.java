77b03dbd5438183db98539663ac60c9bc50b1314
Nida-Krailas <nkrailas@gmail.com>
2018-03-03 00:27:32 -0600

    Reformat Code, Rearrange Code

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 /**
@@ -20,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        CheckBox whippedCream = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = whippedCream.isChecked();
+
         int price = calculatePrice();
-        displayMessage(createOrderSummary(price));
+        displayMessage(createOrderSummary(price, hasWhippedCream));
     }
 
     /**
@@ -37,13 +41,14 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Create summary of the order.
-     *
+     * @param hasWhippedCream is whether or not the use wants whipped cream topping
      * @param price of the order
      * @return text summary
      */
 
-    private String createOrderSummary(int price) {
+    private String createOrderSummary(int price, boolean hasWhippedCream) {
         String priceMessage = "Name: Kaptain Kunal";
+        priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
         priceMessage += "\nQuantity: " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank you!";
@@ -62,12 +67,12 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the minus button is clicked.
      */
-    public void decrement(View view)   {
-        if (quantity > 0)     {
+    public void decrement(View view) {
+        if (quantity > 0) {
             quantity--;    //(same thing as quantity = quantity - 1;)
             displayQuantity(quantity);
         }
-        }
+    }
 
     /**
      * This method displays the given quantity value on the screen.
@@ -85,3 +90,5 @@ public class MainActivity extends AppCompatActivity {
         orderSummaryTextView.setText(message);
     }
 }
+
+
